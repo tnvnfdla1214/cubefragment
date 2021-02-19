03cb74a795f9ba5146c3cac75057427971dda528
Min Gyu <tnvnfdla12@gmail.com>
2021-02-19 15:45:14 +0900

    돌아가는 프레그먼트

--- app/src/main/java/com/labo/kaji/fragmentanimations/example/MainActivity.java
+++ app/src/main/java/com/labo/kaji/fragmentanimations/example/MainActivity.java
@@ -10,7 +10,7 @@ import android.view.MenuItem;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
-
+/////
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
